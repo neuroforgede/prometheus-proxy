14153bfa91ef26457507935df3e680fcc8849ebf
Martin Braun <braun@neuroforge.de>
2023-01-31 02:20:48 +0100

    add support for configuration

--- prometheus-proxy/index.ts
+++ prometheus-proxy/index.ts
@@ -4,6 +4,19 @@ import cors from 'cors';
 import client from 'prom-client';
 import axios from 'axios';
 import { resolve4 } from 'node:dns/promises';
+import fs from 'fs';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const Registry = client.Registry;
+const metricsRegistry = new Registry();
+collectDefaultMetrics({ register: metricsRegistry });
+
+const fetchErrors = new client.Counter({
+    name: 'prometheus_proxy_fetch_errors',
+    help: 'Error count',
+    labelNames: ['proxy_error_instance', 'proxy_error_job']
+});
+metricsRegistry.registerMetric(fetchErrors);
 
 const app = express();
 
@@ -15,10 +28,10 @@ app.get('/healthz', function (req, res) {
     res.send('I am happy and healthy\n');
 });
 
-const regexWithAttributes = new RegExp(/^(\w+)\{(.*)\}\s+(.*)$/, 'gm');
-const regexWithoutAttributes = new RegExp(/^(\w+)\s+(.*)$/, 'gm');
+const regexWithLabels = new RegExp(/^(\w+)\{(.*)\}\s+(.*)$/, 'gm');
+const regexWithoutLabels = new RegExp(/^(\w+)\s+(.*)$/, 'gm');
 
-const config: {
+type Config ={
     apps: {
         job_name: string,
         dns_sd_configs: {
@@ -28,26 +41,11 @@ const config: {
         }[],
         metrics_path?: string
     }[]
-} = {
-    apps: [
-        {
-            job_name: 'prometheus-proxy',
-            dns_sd_configs: [
-                {
-                    names: ['prometheus-proxy'],
-                    type: 'A',
-                    port: '3000'
-                },
-                {
-                    names: ['prometheus-proxy.local'],
-                    type: 'A',
-                    port: '3000'
-                }
-            ],
-            metrics_path: '/metrics'
-        }
-    ]
-}
+};
+
+let config: Config;
+const configContent = fs.readFileSync(process.env.CONFIG_FILE ?? (__dirname + '/config.json')).toString();
+config = JSON.parse(configContent);
 
 app.get('/proxy', async (req, res) => {
     try {
@@ -55,26 +53,41 @@ app.get('/proxy', async (req, res) => {
         res.status(200);
 
         for (const app of config.apps) {
-            const instances = new Set<string>();
-            for (const dnsSdConfig of app.dns_sd_configs) {
-                for (const name of dnsSdConfig.names) {
-                    const dnsResult = await resolve4(name);
-                    for (const ip of dnsResult) {
+            try{
+                const instances = new Set<string>();
+                for (const dnsSdConfig of app.dns_sd_configs) {
+                    for (const name of dnsSdConfig.names) {
+                        const dnsResult = await resolve4(name);
+                        for (const ip of dnsResult) {
 
-                        const instance = `${ip}:${dnsSdConfig.port}`;
-                        instances.add(instance);
+                            const instance = `${ip}:${dnsSdConfig.port}`;
+                            instances.add(instance);
+                        }
                     }
                 }
-            }
-            const metricsPath = app.metrics_path ?? '/metrics'
-            for (const instance of instances) {
-                const remoteMetrics = await axios.get(`http://${instance}${metricsPath}`);
-                const response: string = remoteMetrics.data;
+                const metricsPath = app.metrics_path ?? '/metrics'
+                for (const instance of instances) {
+                    try {
+                        const remoteMetrics = await axios.get(`http://${instance}${metricsPath}`);
+                        const response: string = remoteMetrics.data;
 
-                const augmented =
-                    response.replace(regexWithAttributes, `$1{proxy_instance="${instance}",proxy_job="${app.job_name}",$2} $3`)
-                        .replace(regexWithoutAttributes, `$1{proxy_instance="${instance}",proxy_job="${app.job_name}"} $2`);
-                res.send(augmented);
+                        const augmented =
+                            response.replace(regexWithLabels, `$1{proxy_instance="${instance}",proxy_job="${app.job_name}",$2} $3`)
+                                .replace(regexWithoutLabels, `$1{proxy_instance="${instance}",proxy_job="${app.job_name}"} $2`);
+                        res.send(augmented);
+                    } catch(error) {
+                        console.error(JSON.stringify(error));
+                        fetchErrors.labels({
+                            proxy_error_job: app.job_name,
+                            proxy_error_instance: instance
+                        }).inc();
+                    }
+                }
+            } catch(error) {
+                console.error(JSON.stringify(error));
+                fetchErrors.labels({
+                    proxy_error_job: app.job_name
+                }).inc();
             }
         }
         res.end();
@@ -85,10 +98,6 @@ app.get('/proxy', async (req, res) => {
     }
 });
 
-const collectDefaultMetrics = client.collectDefaultMetrics;
-const Registry = client.Registry;
-const metricsRegistry = new Registry();
-collectDefaultMetrics({ register: metricsRegistry });
 
 // these are our own metrics
 app.get('/metrics', (req, res) => {
